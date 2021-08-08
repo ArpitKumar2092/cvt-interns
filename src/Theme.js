87fc22a36d2cc7e678f6013e5a53538e394242fb
Nimisha-12345 <nimishas082@gmail.com>
2021-08-09 03:12:12 +0530

    changes in material ui

---
+++ src/Theme.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Theme = () => {
+    return (
+        <div>
+            <h1>nimi</h1>
+        </div>
+    )
+}
+
+export default Theme;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createTheme } from '@material-ui/core/styles';
+
+// const theme = createTheme({
+//     palette : {
+//         primary :{
+//             main: "#764abc"
+//         }
+//     },
+//     background:{
+//         default:"#fff"
+//     }
+// })
+
+// export default theme;
