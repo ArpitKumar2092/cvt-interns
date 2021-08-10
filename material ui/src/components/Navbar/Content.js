c7cbe80ef9e972b2cab2d14b99f7d484f470ae27
Nimisha-12345 <nimishas082@gmail.com>
2021-08-11 02:17:37 +0530

    file changes

---
+++ material ui/src/components/Navbar/Content.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles((theme) => ({
+  heroContent: {
+    padding: theme.spacing(8),
+  },
+
+  btn: {
+    background: "#ef5f22",
+    color: "#FFFFFF",
+    marginRight: "20px",
+  },
+
+  btn1: {
+    color: "#ef5f22",
+    background: "#FFFFFF",
+  },
+  heroContentHead: {
+    marginLeft: "131px",
+    marginRight: "113px",
+  },
+  photo: {
+    width: "417px",
+    height: "385px",
+    marginRight: "94px",
+    borderRadius: "inherit",
+  },
+}));
+
+export default function Pricing() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <Container maxWidth="ls" className={classes.heroContent}>
+        <Toolbar>
+          <Typography
+            variant="h2"
+            color="textPrimary"
+            className={classes.heroContentHead}
+          >
+            Supercharging Somali Startups
+            <Typography variant="h5" color="textSecondary" component="p">
+              we supercharge Somali startup by providing traning, mentoring and
+              funding.
+            </Typography>
+            <Button className={classes.btn}>Learn More</Button>
+            <Button className={classes.btn1}>Partner with us</Button>
+          </Typography>
+
+          <Avatar
+            className={classes.photo}
+            src="static/images/avatar/Background.png"
+          />
+        </Toolbar>
+      </Container>
+    </React.Fragment>
+  );
+}
--- material ui/src/components/Navbar/Header.js
+++ material ui/src/components/Navbar/Header.js
@@ -1,19 +1,15 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-
-import CardHeader from "@material-ui/core/CardHeader";
+import Content from "./Content";
+import SubContent from "./SubContent";
+import MenuHeader from "./MenuHeader";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import PolymerIcon from "@material-ui/icons/Polymer";
-import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   "@global": {
@@ -36,77 +32,18 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     wordWrap: "break-word",
   },
-  link: {
-    margin: theme.spacing(3, 2),
-    color: "#858892",
-  },
+
   links: {
     background: "white",
     color: "#A3A5AA",
   },
-  heroContent: {
-    padding: theme.spacing(8),
-  },
-  heroContentHead: {},
-  foot: {
-    padding: theme.spacing(5, 9, 6),
-    background: " #f0f5fc ",
-  },
-  ftile: {},
-  furtile: {
-    padding: "160px",
-  },
-  furtility: {
-    background: "#f0f5fc",
-    boxShadow: "none",
-  },
-  sigma: {
-    color: "#73cae5",
-  },
-  title: {},
-  btn: {
-    background: "#ef5f22",
-    color: "#FFFFFF",
-    marginRight: "20px",
-  },
 
-  btn1: {
-    color: "#ef5f22",
-    background: "#FFFFFF",
-  },
-  heroContentHead: {
-    marginLeft: "131px",
-    marginRight: "113px",
-  },
-  photo: {
-    width: "417px",
-    height: "385px",
-    marginRight: "94px",
-    borderRadius: "inherit",
-  },
+  
 }));
-
-const tiers = [
-  {
-    title: "54+",
-    subheader: "Startups",
-    price: "0",
-  },
-  {
-    title: "13+",
-    subheader: "Mentors",
-    price: "15",
-  },
-  {
-    title: "140+",
-    subheader: "Founder",
-  },
-];
 
 export default function Pricing() {
   const classes = useStyles();
-
-  return (
+   return (
     <React.Fragment>
       <CssBaseline />
       <Grid container justifyContent="center">
@@ -117,102 +54,19 @@ export default function Pricing() {
                 <Toolbar>
                   <PolymerIcon style={{ color: "#EF5F22" }} />
                   Innvote Ventures
-                  <nav>
-                    <Link
-                      variant="button"
-                      color="textPrimary"
-                      href="#"
-                      className={classes.link}
-                    >
-                      About us
-                    </Link>
-                    <Link
-                      variant="button"
-                      color="textPrimary"
-                      href="#"
-                      className={classes.link}
-                    >
-                      Programmers
-                    </Link>
-                    <Link
-                      variant="button"
-                      color="textPrimary"
-                      href="#"
-                      className={classes.link}
-                    >
-                      Startup
-                    </Link>
-                    <Link
-                      variant="button"
-                      color="textPrimary"
-                      href="#"
-                      className={classes.link}
-                    >
-                      Events
-                    </Link>
-                  </nav>
+                  <MenuHeader />
                 </Toolbar>
               </Typography>
-
-              <Toolbar></Toolbar>
-
-              <Button href="#" className={classes.links}>
+             <Button href="#" className={classes.links}>
                 Apply for 2019
               </Button>
             </Toolbar>
           </AppBar>
           {/* Hero unit */}
-          <Container maxWidth="ls" className={classes.heroContent}>
-            <Toolbar>
-              <Typography
-                // component="h1"
-                variant="h2"
-                color="textPrimary"
-                className={classes.heroContentHead}
-              >
-                Supercharging Somali Startups
-                <Typography variant="h5" color="textSecondary" component="p">
-                  we supercharge Somali startup by providing traning, mentoring
-                  and funding.
-                </Typography>
-                <Button className={classes.btn}>Learn More</Button>
-                <Button className={classes.btn1}>Partner with us</Button>
-              </Typography>
-
-              <Avatar
-                className={classes.photo}
-                src="static/images/avatar/Background.png"
-              />
-            </Toolbar>
-          </Container>
+          <Content />
 
           {/* End hero unit */}
-          <Container maxWidth="ls" component="main" className={classes.foot}>
-            <Grid
-              container
-              spacing={6}
-              alignItems="flex-end"
-              className={classes.ftile}
-            >
-              {tiers.map((tier) => (
-                <Grid item key={tier.title} md={4} className={classes.furtile}>
-                  <Card className={classes.furtility}>
-                    <CardHeader
-                      className={classes.sigma}
-                      title={tier.title}
-                      subheader={tier.subheader}
-                      titleTypographyProps={{
-                        align: "center",
-                        variant: "h1",
-                        fontWeight: "600",
-                      }}
-                      subheaderTypographyProps={{ align: "center" }}
-                    />
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
+          <SubContent />
         </Grid>
       </Grid>
     </React.Fragment>
